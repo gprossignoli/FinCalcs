809f11e10a8baba13da36efc71c9a7772f068a4e
gprossignoli <gprauxiliar@gmail.com>
2021-04-16 17:25:49 +0200

    ADD: calmar ratio

--- src/Portfolio/domain/portfolio.py
+++ src/Portfolio/domain/portfolio.py
@@ -36,6 +36,17 @@ class Portfolio:
                             / self.weighted_returns[1]
         return (1 + cumulative_return)**(365/st.ANNUALIZATION_FACTOR) - 1
 
+    @property
+    def mdd(self):
+        """
+        Max Drawdown
+        """
+        cum_rets = self.weighted_returns.add(1).cumprod()
+        nav = ((1 + cum_rets) * 100).fillna(100)
+        hwm = nav.cummax()
+        dd = nav / hwm - 1
+        return min(dd)
+
     def sharpe_ratio(self):
         return (self.annualized_returns - st.RISK_FREE_RATIO) / self.annualized_volatility
 
@@ -43,6 +54,9 @@ class Portfolio:
         neg_asset_returns = (self.weighted_returns[self.weighted_returns < 0][self.first_date:self.last_date])
         std_dev = neg_asset_returns.std()
         return (self.annualized_returns - benchmark_returns) / (std_dev * math.sqrt(st.ANNUALIZATION_FACTOR))
+
+    def calmar_ratio(self):
+        return (self.annualized_returns - st.RISK_FREE_RATIO) / self.mdd
 
     def __compute_common_date(self) -> tuple:
         common_idx = self.symbols[0].daily_returns.index
